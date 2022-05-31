9bc261891c2c1906cb8f6e4c01ebf605284985ec
Tonye Jack <jtonye@ymail.com>
2022-05-30 22:19:52 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -64,7 +64,7 @@ export async function run(): Promise<void> {
   const baseSha = core.getInput('base-sha', {required: includeDeletedFiles})
 
   const workingDirectory = path.resolve(
-    process.env.GITHUB_WORKSPACE!,
+    process.env.GITHUB_WORKSPACE || process.cwd(),
     core.getInput('working-directory', {required: true})
   )
 
