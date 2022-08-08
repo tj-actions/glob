081b4c8e35a9b97f90e0d08b6fb724a2401ca250
Tonye Jack <jtonye@ymail.com>
2022-08-08 10:39:24 -0600

    update variable name.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import * as path from 'path'
 import * as core from '@actions/core'
 import * as glob from '@actions/glob'
 import {promises as fs} from 'fs'
-import isGlob from 'is-glob';
+import isGlob from 'is-glob'
 
 import {
   getDeletedFiles,
@@ -181,20 +181,23 @@ export async function run(): Promise<void> {
     core.saveState('paths-output-file', pathsOutputFile)
     core.info(`Successfully created paths-output-file: ${pathsOutputFile}`)
   } else {
-    const allPatterns = filePatterns
+    const defaultExcludedPatterns = DEFAULT_EXCLUDED_FILES.map(
+      ep => `!${path.join(workingDirectory, ep.replace(/^!/, ''))}`
+    )
+    const invalidPatterns = filePatterns
       .split('\n')
       .filter(
         p =>
-          !DEFAULT_EXCLUDED_FILES.map(
-            ep => `!${path.join(workingDirectory, ep.replace(/^!/, ''))}`
-          ).includes(p) &&
           p !== '' &&
-          !isGlob(p, {strict: false})
+          !p.startsWith('!') &&
+          isGlob(p, {strict: false}) &&
+          !defaultExcludedPatterns.includes(p)
       )
 
-    if (allPatterns.length > 0) {
+    if (invalidPatterns.length > 0) {
       core.warning(
-        'No match found for specified patterns. Ensure that subdirectory patterns are prefixed with "**/" and all multi line string patterns are specified without quotes. See: https://docs.github.com/en/actions/using-workflows/workflow-syntax-for-github-actions#filter-pattern-cheat-sheet'
+        `Invalid pattern detected: "${invalidPatterns.join(', ')}". 
+        Ensure that subdirectory patterns are prefixed with "**/" and all multi line string patterns are specified without quotes. See: https://docs.github.com/en/actions/using-workflows/workflow-syntax-for-github-actions#filter-pattern-cheat-sheet`
       )
     }
   }
